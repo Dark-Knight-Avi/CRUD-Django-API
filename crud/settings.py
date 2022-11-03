bad0bda6d4c2b26572a60dbaf34ac401d47ee897
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2022-11-03 23:43:17 +0530

    storage

--- crud/settings.py
+++ crud/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
